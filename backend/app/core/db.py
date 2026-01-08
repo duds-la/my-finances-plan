e82670f12babb1d38ba9746ee2f6543e18bf8480
Eduardo A. Lazzaretti <duadsal@gmail.com>
2026-01-08 20:43:31 -0300

    Configuring dependencies and modifying the initial project setup

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,8 +1,8 @@
 from sqlmodel import Session, create_engine, select
 
-from app import crud
+#from app import crud
 from app.core.config import settings
-from app.models import User, UserCreate
+#from app.models import User, UserCreate
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
@@ -12,7 +12,7 @@ engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 # for more details: https://github.com/fastapi/full-stack-fastapi-template/issues/28
 
 
-def init_db(session: Session) -> None:
+#def init_db(session: Session) -> None:
     # Tables should be created with Alembic migrations
     # But if you don't want to use migrations, create
     # the tables un-commenting the next lines
@@ -21,13 +21,13 @@ def init_db(session: Session) -> None:
     # This works because the models are already imported and registered from app.models
     # SQLModel.metadata.create_all(engine)
 
-    user = session.exec(
-        select(User).where(User.email == settings.FIRST_SUPERUSER)
-    ).first()
-    if not user:
-        user_in = UserCreate(
-            email=settings.FIRST_SUPERUSER,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            is_superuser=True,
-        )
-        user = crud.create_user(session=session, user_create=user_in)
+    #user = session.exec(
+    #    select(User).where(User.email == settings.FIRST_SUPERUSER)
+    #).first()
+    #if not user:
+    #    user_in = UserCreate(
+    #        email=settings.FIRST_SUPERUSER,
+    #        password=settings.FIRST_SUPERUSER_PASSWORD,
+    #        is_superuser=True,
+    #    )
+    #    user = crud.create_user(session=session, user_create=user_in)
