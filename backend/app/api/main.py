876fc0ab4ec43c66847ed06e8df75bd83e7b6379
Eduardo A. Lazzaretti <duadsal@gmail.com>
2026-04-10 15:04:17 -0300

    Creating transaction_type CRUD: CREATE

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.routes import *
+from app.api.routes import login, transaction_type_route
 from app.core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(login.router)
+api_router.include_router(transaction_type_route.router)
 
 
 #if settings.ENVIRONMENT == "local":
