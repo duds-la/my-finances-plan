503abbdb085550781e112dee84b69cad383367fd
Eduardo A. Lazzaretti <duadsal@gmail.com>
2026-04-10 15:04:56 -0300

    Creating transaction_type CRUD: CREATE

---
+++ backend/app/api/routes/transaction_type_route.py
@@ -0,0 +1,60 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List
+
+from app.database.session import get_db
+from app.schemas.transaction_type import (
+    Transaction_Type_Create,
+    Transaction_Type_Update,
+    Transaction_Type_Response
+)
+from app.repositories.transaction_type_repository import Transaction_Type_Repository
+
+router = APIRouter(prefix="/transaction_type", tags=["transaction_type"])
+repository = Transaction_Type_Repository()
+
+@router.post("/", response_model=Transaction_Type_Response, status_code=status.HTTP_201_CREATED)
+def create(data: Transaction_Type_Create, db: Session = Depends(get_db)):
+    return repository.create(db, data.model_dump())
+
+@router.get("/", response_model=List[Transaction_Type_Response])
+def get_all(db: Session = Depends(get_db)):
+    return repository.get_all(db)
+
+@router.get("/{id}", response_model=Transaction_Type_Response)
+def get_by_id(id: int, db: Session = Depends(get_db)):
+    obj = repository.get_by_id(db, id)
+
+    if not obj:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Transaction Type whith code: {id} not found!"
+        )
+    
+    return obj
+
+@router.patch("/{id}", response_model=Transaction_Type_Response)
+def update(id:int, data: Transaction_Type_Update, db: Session = Depends(get_db)):
+    obj = repository.get_by_id(db, id)
+
+    if not obj:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Transaction Type whith code: {id} not found!"
+        )
+    
+    update_data = data.model_dump(exclude_unset=True)
+
+    return repository.get_by_id(db, id)
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, db: Session = Depends(get_db)):
+    obj = repository.get_by_id(db, id)
+ 
+    if not obj:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Transaction type with id {id} not found"
+        )
+ 
+    repository.delete(db, obj)
